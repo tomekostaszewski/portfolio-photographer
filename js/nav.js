13031dc8a7d7f8e6307f78f59f21d4a97e99c752
Tomek <ostaszewski.tomek@wp.pl>
2021-09-28 07:01:34 +0200

    Added website - Portfolio photographer. Website v1.0

---
+++ js/nav.js
@@ -0,0 +1,29 @@
+const navBtn = document.querySelector('.bars-btn');
+const nav = document.querySelector('.nav-container');
+const items = document.querySelectorAll('.item');
+const navTop = document.querySelector('nav');
+
+const openNav = () => {
+    nav.classList.toggle('active')
+
+    items.forEach(item => {
+        item.addEventListener('click', () => {
+            nav.classList.remove('active');
+        })
+    })
+}
+
+navBtn.addEventListener('click', openNav);
+
+
+
+const fixedNav = () => {
+    if (window.scrollY >= 10) {
+        navTop.classList.add('fixNav');
+    } else {
+        navTop.classList.remove('fixNav')
+    }
+}
+
+window.addEventListener('scroll', fixedNav);
+
---
+++ js/option_light_box.js
@@ -0,0 +1,7 @@
+lightbox.option({
+    'resizeDuration': 200,
+    'wrapAround': true,
+    'alwaysShowNavOnTouchDevices': true,
+    'fadeDuration': true,
+    'showImageNumberLabel': false
+})
